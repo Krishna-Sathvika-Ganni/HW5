0eeaa31b21b756faf7c43ed8d3141eb42f5d76f6
Krisa29 <sathvika.ganni@gmail.com>
2025-02-24 21:36:43 -0500

    Modified test_commands

--- tests/test_command.py
+++ tests/test_command.py
@@ -9,37 +9,38 @@ from app.commands.menucommand import Menu
 def test_add_command(capfd):
     command=Add()
     command.execute('2','3')
-    out, err=capfd.readouterr()
-    assert "2 + 3 = 5" in out, "addcommand should add two numbers"
+    out, _=capfd.readouterr()
+    assert "2 + 3 = 5" in out, "AddCommand should add two numbers"
 
 def test_subtract_command(capfd):
     command=Subtract()
     command.execute('3','2')
-    out, err=capfd.readouterr()
-    assert "3 - 2 = 1" in out, "subtractcommand should subtract two numbers"
+    out, _=capfd.readouterr()
+    assert "3 - 2 = 1" in out, "SubtractCommand should subtract two numbers"
 
 def test_multiply_command(capfd):
     command=Multiply()
     command.execute('2','3')
-    out, err=capfd.readouterr()
-    assert "2 * 3 = 6" in out, "multiplycommand should multiply two numbers"
+    out, _=capfd.readouterr()
+    assert "2 * 3 = 6" in out, "MultiplyCommand should multiply two numbers"
 
 def test_divide_command(capfd):
     command=Divide()
     command.execute('4','2')
-    out, err=capfd.readouterr()
-    assert "4 / 2 = 2" in out, "dividecommand should divide two numbers"
+    out, _=capfd.readouterr()
+    assert "4 / 2 = 2" in out, "DivideCommand should divide two numbers"
 
 def test_divide_by_zero(capfd):
     command=Divide()
     command.execute('2','0')
-    out, err=capfd.readouterr()
-    assert "Division by zero cannot happen" in out, "dividecommand should handle division by zero"
+    out, _=capfd.readouterr()
+    assert "Division by zero cannot happen" in out, "DivideCommand should handle division by zero"
 
 def test_menu_command(capfd):
-    command=Menu()
+    command_handler={}
+    command=Menu(command_handler)
     command.execute()
-    out, err=capfd.readouterr()
-    assert "Available commands:" in out, "menucommand should display the available commands"
+    out, _=capfd.readouterr()
+    assert "Available commands:" in out, "MenuCommand should display available commands"
 
 
