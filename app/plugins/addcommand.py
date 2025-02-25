693807319441aaba44c3118637ef05c56841f2a2
Krisa29 <sathvika.ganni@gmail.com>
2025-02-25 00:03:44 -0500

    Shifted add,subtract, multiply,divide, menu commands from commands folder to plugins

---
+++ app/plugins/addcommand.py
@@ -0,0 +1,20 @@
+from decimal import Decimal, InvalidOperation
+from app.calculator import Calculator
+from app.commands import Command
+
+class Add(Command):
+    def execute(self, *args):
+        if not args:  # This prompts for input, if arguments are not given
+            args = input("Enter two numbers separated by space: ").split()
+        if len(args) != 2:  # Makes sure that only two arguments must be given
+            print("Only two arguments must be given")
+            return
+
+        try:
+            x, y = map(Decimal, args)  
+            result = Calculator.add(x, y)
+            print(f"{x} + {y} = {result}")
+        except InvalidOperation:
+            print(f"One of the entered numbers is invalid. Please enter valid inputs.")
+        except Exception as e:
+            print(f"{e}")
