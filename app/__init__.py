0ef6430e3b0e4aaac7c03ec4f6909c5fbee8c13c
Krisa29 <sathvika.ganni@gmail.com>
2025-02-24 23:24:37 -0500

    Added plugins

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,30 @@
-from app.commands.addcommand import Add 
-from app.commands.subtractcommand import Subtract
-from app.commands.multiplycommand import Multiply
-from app.commands.dividecommand import Divide
-from app.commands.menucommand import Menu
+import pkgutil
+import importlib
+import inspect
 from app.commands import CommandHandler
+from app.commands import Command
 
 class App:
     def __init__(self):
         self.command_handler=CommandHandler()
-        self.command_handler.Register_Command("Add", Add())
-        self.command_handler.Register_Command("Subtract",Subtract())
-        self.command_handler.Register_Command("Multiply", Multiply())
-        self.command_handler.Register_Command("Divide", Divide())
-        self.command_handler.Register_Command("Menu", Menu(self.command_handler))
+        self.load_plugins() # Loading plugins dynamically
 
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
     def start(self):
         print("This is the calculator program. Enter Menu to see the commands available")
-        self.command_handler.Execute_Command("Menu")
-        while True:
-            c=input("Enter the command:").strip()
-            if c.lower() == "exit":
-                print("Exiting..")
-                break
-            else:
-                self.command_handler.Execute_Command(*c.split())
+        self.load_plugins()
+        print("Type 'exit' to exit.")
+        while True:  #REPL Read, Evaluate, Print, Loop
+            self.command_handler.execute_command(input(">>> ").strip())
